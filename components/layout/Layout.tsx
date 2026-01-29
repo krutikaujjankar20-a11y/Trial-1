f16b4fbd728c00b9bda1e65256285f848d73246b
krutikaujjankar20-a11y <krutikaujjankar20@gmail.com>
2026-01-29 21:11:02 +0530

    Upload files from zip

---
+++ components/layout/Layout.tsx
@@ -0,0 +1,22 @@
+
+import React, { useState } from 'react';
+import { Outlet } from 'react-router-dom';
+import { Sidebar } from './Sidebar';
+import { Header } from './Header';
+
+export const Layout: React.FC = () => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
+  return (
+    <div className="flex min-h-screen bg-gray-50">
+      <Sidebar isOpen={isSidebarOpen} toggle={() => setIsSidebarOpen(!isSidebarOpen)} />
+      
+      <div className="flex-1 flex flex-col min-w-0">
+        <Header onMenuClick={() => setIsSidebarOpen(true)} />
+        <main className="flex-1 p-4 md:p-8 max-w-7xl mx-auto w-full">
+          <Outlet />
+        </main>
+      </div>
+    </div>
+  );
+};
